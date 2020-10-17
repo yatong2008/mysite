67fa950535862b1df4619455278b12bf2d665b1d
Yatong Zhou <Mike.z@anzuk.education>
2020-10-17 19:32:40 +1100

    use localStorage to save the cart items

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,88 +1,100 @@
 <template>
-    <div>
-        <h2>{{title}}</h2>
-        <table border="1">
-            <tr>
-                <th>#</th>
-                <th>Course</th>
-                <th>Item price</th>
-                <th>Quantity</th>
-                <th>Total Price</th>
-            </tr>
-            <tr  v-for='(c,index) in cart' :key='c.id'>
-                <td>
-                    <input type="checkbox" v-model='c.active' />
-                </td>
-                <td>{{c.title}}</td>
-                <td>{{c.price}}</td>
-                <td>
-                    <button @click="subtract(index)">-</button>
-                    {{c.count}}
-                    <button @click="add(index)">+</button>
-                </td>                
-                <td>{{c.price * c.count}}</td>
+  <div>
+    <h2>{{ title }}</h2>
+    <table border="1">
+      <tr>
+        <th>#</th>
+        <th>Course</th>
+        <th>Item price</th>
+        <th>Quantity</th>
+        <th>Total Price</th>
+      </tr>
+      <tr v-for='(c,index) in cart' :key='c.id'>
+        <td>
+          <input type="checkbox" v-model='c.active'/>
+        </td>
+        <td>{{ c.title }}</td>
+        <td>{{ c.price }}</td>
+        <td>
+          <button @click="subtract(index)">-</button>
+          {{ c.count }}
+          <button @click="add(index)">+</button>
+        </td>
+        <td>{{ c.price * c.count }}</td>
 
-            </tr>
-            <tr>
-                <td></td>
-                <td colspan="2">{{activeCount}}/{{count}}</td>
-                <td colspan="2">{{total}}</td>
-            </tr>            
-        </table>
-    </div>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="2">{{ activeCount }}/{{ count }}</td>
+        <td colspan="2">{{ total }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "cart",
-        props:['title'],
-      data() {
-          return {
-            cart: []
-          }
-      },
-      created() {
-          this.$bus.$on('addCart', good=>{
-            const ret = this.cart.find(v=> v.id===good.id);
-            if(!ret) {
-              //cart does not contain the data
-              this.cart.push(good);
-            } else {
-
-              ret.count++;
-            }
-          });
-      },
-      methods: {
-            remove(i) {
-                if(window.confirm('Are you sure?')) {
-                    this.cart.splice(i,1);
-                }
-            },
-            add(i) {
-                this.cart[i].count++;
-            },
-            subtract(i) {
-                let count = this.cart[i].count;
-                count > 1 ? this.cart[i].count -= 1 : this.remove(i);
-            }
-        },
-        computed: {
-            count() {
-                return this.cart.length
-            },
-            activeCount() {
-                return this.cart.filter(v => v.active).length
-            },
-            total() {
-                return this.cart.reduce((sum, c) => {
-                    if(c.active)
-                        sum += c.price * c.count;
-                    return sum;
-                }, 0)
-            }
-        }
+export default {
+  name: "cart",
+  props: ['title'],
+  data() {
+    return {
+      cart: JSON.parse(localStorage.getItem('cart')) || []
     }
+  },
+  watch: {
+    cart: {
+      // eslint-disable-next-line no-unused-vars
+      handler(n, o) {
+        this.setLocalData(n);
+      },
+      deep: true
+    }
+  },
+  created() {
+    this.$bus.$on('addCart', good => {
+      const ret = this.cart.find(v => v.id === good.id);
+      if (!ret) {
+        //cart does not contain the data
+        this.cart.push(good);
+      } else {
+
+        ret.count++;
+      }
+    });
+  },
+  methods: {
+    setLocalData(n) {
+      localStorage.setItem('cart', JSON.stringify(n));
+    },
+    remove(i) {
+      if (window.confirm('Are you sure?')) {
+        this.cart.splice(i, 1);
+      }
+    },
+    add(i) {
+      this.cart[i].count++;
+    },
+    subtract(i) {
+      let count = this.cart[i].count;
+      count > 1 ? this.cart[i].count -= 1 : this.remove(i);
+    }
+  },
+  computed: {
+    count() {
+      return this.cart.length
+    },
+    activeCount() {
+      return this.cart.filter(v => v.active).length
+    },
+    total() {
+      return this.cart.reduce((sum, c) => {
+        if (c.active)
+          sum += c.price * c.count;
+        return sum;
+      }, 0)
+    }
+  }
+}
 </script>
 
 <style scoped>
